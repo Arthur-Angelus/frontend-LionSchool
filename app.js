c027d145bb26f3ddde14c965e3dd94b5faf07928
ARTHUR ANGELUS ANDRADE DE ALMEIDA <25132783@CFP127ALUNO.LOCAL>
2026-02-27 11:16:54 -0300

    iniciando a função e forma para aparecer as informações de um aluno ao clicar no card

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ function aparecerAlunos() {
   const botaoDs = document.getElementById('ds');
   const rd = document.getElementById('alunos-rd')
   const botaoRd = document.getElementById('rd');
+  const alunoInfo = document.getElementById('aluno')
+  const botaoAluno = document.getElementById('card');
 
 
   botaoDs.addEventListener('click', function () {
@@ -23,12 +25,24 @@ function aparecerAlunos() {
   botaoRd.addEventListener('click', function () {
     rd.style.display = 'flex';
     principal.style.display = 'none';
+    const container2 = document.getElementById('cards2')
+    container2.innerHTML = '' // limpa antes de cria
+
+    if (container2) {
+      alunosRd(container2)
+  }
   });
 
   botaoSair.addEventListener('click', function () {
     rd.style.display = 'none';
     ds.style.display = 'none';
     principal.style.display = 'flex';
+  });
+
+  botaoAluno.addEventListener('click', function () {
+    rd.style.display = 'none';
+    ds.style.display = 'none';
+    alunoInfo.style.display = 'flex';
   });
 }
 
@@ -47,7 +61,38 @@ async function alunosDs(container) {
           <img src="${pessoa.foto}" alt="Foto de ${pessoa.nome}">
           <p>${pessoa.nome}</p>
         `
+        card.addEventListener('click', () => {
+          alunoInformacao(pessoa)
+        })
+
       container.appendChild(card)
+    })
+
+  } catch (erro) {
+    console.error('Erro ao carregar pessoas:', erro)
+  }
+}
+
+async function alunosRd(container2) {
+  const url = 'https://lion-school-backend.onrender.com/alunos?curso_id=2'
+
+  try {
+    const response = await fetch(url)
+    const dados = await response.json()
+
+    dados.forEach(pessoa => {
+      const card = document.createElement('button')
+      card.classList.add('card')
+
+      card.innerHTML = `
+          <img src="${pessoa.foto}" alt="Foto de ${pessoa.nome}">
+          <p>${pessoa.nome}</p>
+        `
+        card.addEventListener('click', () => {
+          alunoInformacao(pessoa)
+        })
+
+      container2.appendChild(card)
     })
 
   } catch (erro) {
@@ -56,7 +101,7 @@ async function alunosDs(container) {
 
 
 }
-function alunoInformacao() {
+function alunoInformacao(pessoa) {
 
 }
 

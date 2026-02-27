42744c3099d5f604239401edf3414f8d3c9ebdd6
ARTHUR ANGELUS ANDRADE DE ALMEIDA <25132783@CFP127ALUNO.LOCAL>
2026-02-27 09:29:05 -0300

    criando a função para criar os cards de ds

--- app.js
+++ app.js
@@ -1,42 +1,63 @@
 'use strict'
 
-document.addEventListener("DOMContentLoaded", function () {
-    aparecerAlunos();
+function aparecerAlunos() {
+  const botaoSair = document.getElementById('sair');
+  const principal = document.getElementById('principal')
+  const ds = document.getElementById('alunos-ds');
+  const botaoDs = document.getElementById('ds');
+  const rd = document.getElementById('alunos-rd')
+  const botaoRd = document.getElementById('rd');
+
+
+  botaoDs.addEventListener('click', function () {
+    ds.style.display = 'flex';
+    principal.style.display = 'none';
+    const container = document.getElementById('cards')
+    container.innerHTML = '' // limpa antes de cria
+
+    if (container) {
+      alunosDs(container)
+  }
   });
 
-function aparecerAlunos(){
-    const botaoSair = document.getElementById('sair');
-    const principal = document.getElementById('principal')
-    const ds = document.getElementById('alunos-ds');
-    const botaoDs = document.getElementById('ds');
-    const rd = document.getElementById('alunos-rd')
-    const botaoRd = document.getElementById('rd');
+  botaoRd.addEventListener('click', function () {
+    rd.style.display = 'flex';
+    principal.style.display = 'none';
+  });
 
-
-    botaoDs.addEventListener('click', function() {
-        ds.style.display = 'flex'; 
-        principal.style.display = 'none';
-      });
-
-      botaoRd.addEventListener('click', function() {
-        rd.style.display = 'flex'; 
-        principal.style.display = 'none';
-      });
-
-      botaoSair.addEventListener('click', function() {
-        rd.style.display = 'none';
-        ds.style.display = 'none';
-        principal.style.display = 'flex';
-      });
+  botaoSair.addEventListener('click', function () {
+    rd.style.display = 'none';
+    ds.style.display = 'none';
+    principal.style.display = 'flex';
+  });
 }
 
-async function alunosDs(params) {
-    const response = await fetch('https://lion-school-backend.onrender.com/alunos?curso_id=1')
-    const cards = document.getElementById('cards')
-    const card = document.createElement('card')
+async function alunosDs(container) {
+  const url = 'https://lion-school-backend.onrender.com/alunos?curso_id=1'
+
+  try {
+    const response = await fetch(url)
+    const dados = await response.json()
+
+    dados.forEach(pessoa => {
+      const card = document.createElement('button')
+      card.classList.add('card')
+
+      card.innerHTML = `
+          <img src="${pessoa.foto}" alt="Foto de ${pessoa.nome}">
+          <p>${pessoa.nome}</p>
+        `
+      container.appendChild(card)
+    })
+
+  } catch (erro) {
+    console.error('Erro ao carregar pessoas:', erro)
+  }
+
+
 }
-function alunoInformacao(){
-    const botaoInfo = document.getElementById('alunoInfo')
+function alunoInformacao() {
+
 }
 
 aparecerAlunos();
